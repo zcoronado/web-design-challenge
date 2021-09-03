eb8913ce95f62a27f93a9f68a195553f8c0cd9fd
Zaira Coronado <coronado.zaira@gmail.com>
2021-09-02 23:25:04 -0700

    app.py fix

--- Static/app.py
+++ Static/app.py
@@ -6,17 +6,13 @@ app = Flask(__name__)
 
 
 # 3. Define what to do when a user hits the index route
-@app.route("/")
+@app.route("/index.html")
 def home():
     print("Server received request for 'Home' page...")
     return "Welcome to my 'Home' page!"
 
 
 # 4. Define what to do when a user hits the /about route
-@app.route("/about")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
 
 
 if __name__ == "__main__":
